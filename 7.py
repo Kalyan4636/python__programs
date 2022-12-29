d6fe80ef007c274040f05b3e7cb3e0158255af96
ADITYA KALYAN <79601235+Kalyan4636@users.noreply.github.com>
2022-12-29 21:58:54 +0530

    CODE___ADDED

---
+++ 7.py
@@ -0,0 +1,9 @@
+ # Write a Python program to remove spaces from a given string.
+
+def remove_spaces(str1):
+  str1 = str1.replace(' ','')
+  return str1
+   
+print(remove_spaces("Welcome To Simple Coding"))
+print(remove_spaces("a b c")) 
+ 
